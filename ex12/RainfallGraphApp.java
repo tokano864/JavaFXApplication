0419f2bf1dc6c7df7a75f84152a5a68201dbd833
tokano864 <55218941+tokano864@users.noreply.github.com>
2020-10-23 13:57:52 +0900

    Update RainfallGraphApp.java

--- ex12/RainfallGraphApp.java
+++ ex12/RainfallGraphApp.java
@@ -20,15 +20,13 @@ import javafx.stage.Stage;
 
 public class RainfallGraphApp extends Application {
 
-    public static String gakuban = "18EC025"; // 学籍番号を入力すること
-    public static String yourname = "岡野友美"; // 氏名を入力すること
 
     private File file;
     private int year;
     private int column;
 
     public double[] selectData(File csv, int year, int column) {
-        /* 以下に課題12-1のコードを記述 */
+ 
     	double[] data=new double[12];
     	try {
     		FileInputStream fis = new FileInputStream(file);
@@ -50,7 +48,6 @@ public class RainfallGraphApp extends Application {
     }
 
     public PieChart makePieChart(double[] data) {
-        /* 以下に課題12-2のコードを記述 */
     	PieChart pieChart = new PieChart();
     	for(int i=0;i<12;i++) {
     		pieChart.getData().add(new PieChart.Data((Integer.toString(i+1)+"月"), data[i]));
@@ -131,18 +128,3 @@ public class RainfallGraphApp extends Application {
         System.out.println("完了--RailfallGraphApp");
     }
 }
-
-/* 検証 -- 動作確認テストの内容と方法を記述
-まずファイルを選択し、年を2010、都市を東京を選択し円グラフを表示させる。
-このとき、円グラフが1月から12月まで順に、真上を起点として右回りに表示されているか確認する。
-また、この時にラベル表示が正しく表示されているか確認する。以上のことが確認できたならば円グラフを正しく表示することができたと言える。
-次に年を2011から2019まで順次変更し、表示する。
-これによって、値が変わった時にもう一度データの読みこみをし、円グラフを正常に表示しなおすのを確認するとともに、年を変えた時に表示されるデータが指定した年に対応しているか確認できる。
-同じように都市を札幌、福岡に変更し、年を2010年から2019年を順に選択し、円グラフが正しいデータで表示されるかを確認する。
- */
-
-/* 考察 -- 調査したこと、考慮したこと、工夫したことを記述
-課題1について、ファイルの文字の読み込みについて、文字コードを指定することにより実行するソフトによって文字化けすることを防いだ。
-課題2について、PieChartの作成の仕方や開始位置を真上にするコード、右周りに値を表示させるやり方等を調べた。また、円グラフはEclipseでは初期に右回りに表示されたが、他のソフト
-でどのように表示されているかわからないので、右回りに表示するというコードを記入した。また、ラベルが見やすいようい、ラベルの間隔を20にした。
- */
